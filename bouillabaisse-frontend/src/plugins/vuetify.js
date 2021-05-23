6e79dc623ae5f9651604c215fe0e749821959bc9
Italo <italoviske@hotmail.com>
2021-05-23 12:45:04 -0300

    replaced usage of mdi font with npm package installation

--- bouillabaisse-frontend/src/plugins/vuetify.js
+++ bouillabaisse-frontend/src/plugins/vuetify.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
+import '@mdi/font/css/materialdesignicons.css'
 
 
 Vue.use(Vuetify)
